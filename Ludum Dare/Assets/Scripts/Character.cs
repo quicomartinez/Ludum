5db648710f6242c144b7ca2515f19a256a2a0873
Francisco Martinez <quico_martinez@hotmail.com>
2020-04-18 09:08:50 +0200

    character animation; bars;

---
+++ Ludum Dare/Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Controller controller;
+    private Animator animator;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private void Awake()
+    {
+        controller = GetComponent<Controller>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Movement();
+        Interaction();
+    }
+
+    private void Movement()
+    {
+        Vector2 direction = controller.GetDirection();
+
+        if (direction.magnitude > 0.2f)
+        {
+            transform.position += (Vector3)direction * Time.deltaTime * moveSpeed;
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+    }
+
+    private void Interaction()
+    {
+        if (controller.interactPressed)
+        {
+            Debug.Log("APRETADO");
+        }
+    }
+}
---
+++ Ludum Dare/Assets/Scripts/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    //Toman los valores del Input Manager
+    private string horizontalAxis = "Horizontal";
+    private string verticalAxis = "Vertical";
+    private string interactButton = "interact";
+
+
+    //Variables que usamos para el controlador
+    private float horizontal;
+    private float vertical;
+    public bool interactPressed;
+
+
+    private void Update()
+    {
+        interactPressed = Input.GetButtonDown(interactButton);
+        horizontal = Input.GetAxis(horizontalAxis);
+        vertical = Input.GetAxis(verticalAxis);
+    }
+
+    //Devuelve un vector con la dirección de movimiento
+    public Vector2 GetDirection()
+    {
+        return new Vector2(horizontal, vertical);
+    }
+}
